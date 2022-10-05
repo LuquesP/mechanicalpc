e7c2b4be2e5668ce552833243533d3ce04d46b51
Lukas Pilsl <Lukas.jo.Pilsl@gmail.com>
2022-10-05 22:28:17 +0200

    .

--- src/networks/pct/init_model.py
+++ src/networks/pct/init_model.py
@@ -4,13 +4,13 @@ from networks.pct.pct import Point_Transformer
 def pct_get_model():
     config = {
         "num_points": 2048,
-        "batch_size": 64,
+        "batch_size": 16,
         "use_normals": False,
         "optimizer": "Adam",
         "lr": 0.001,
         "decay_rate": 1e-06,
         "epochs": 30,
-        "num_classes": 62,
+        "num_classes": 68,
         "dropout": 0.4,
         "M": 4,
         "K": 64,
