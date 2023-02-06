5b9be0a044c5ee3d03797a6c083cae24f16c5fc7
lukasp <40497910+LuquesP@users.noreply.github.com>
2023-02-06 22:23:47 +0100

    Update ballquery_cuda.cpp

--- src/networks/repsurf/modules/pointops/src/ballquery/ballquery_cuda.cpp
+++ src/networks/repsurf/modules/pointops/src/ballquery/ballquery_cuda.cpp
@@ -1,14 +1,17 @@
 #include <torch/serialize/tensor.h>
 #include <vector>
-#include <THC/THC.h>
+// #include <THC/THC.h>
+#include <ATen/cuda/CUDAContext.h>
+#include <ATen/cuda/CUDAEvent.h>
+
 #include <ATen/cuda/CUDAContext.h>
 
 #include "ballquery_cuda_kernel.h"
 
-extern THCState *state;
+// extern THCState *state;
 
-#define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x, " must be a CUDAtensor ")
-#define CHECK_CONTIGUOUS(x) AT_CHECK(x.is_contiguous(), #x, " must be contiguous ")
+#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x, " must be a CUDAtensor ")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x, " must be contiguous ")
 #define CHECK_INPUT(x) CHECK_CUDA(x);CHECK_CONTIGUOUS(x)
 
 void ballquery_cuda(int b, int n, int m, float radius, int nsample, at::Tensor new_xyz_tensor, at::Tensor xyz_tensor, at::Tensor idx_tensor)
